9af814173d5979cf8900a6f1a9ae9dfb8dccddf0
bi-Tech2 <jodicksonjoshua@gmail.com>
2025-04-23 11:39:36 +0100

    path uncommented, classname:: classvalue

--- frontend/src/components/ui/ListContainer.tsx
+++ frontend/src/components/ui/ListContainer.tsx
@@ -1,28 +1,29 @@
 import { JSX } from "react";
-// import { Link } from "react-router";
+import { Link } from "react-router";
 import GroupLoadingSkeleton from "../skeletons/CardsLoadingSkeleton";
+import { ClassValue } from "clsx";
 
 interface ListContainerTypes {
   header: string;
   render: JSX.Element[];
   path: string;
   isLoading?: boolean;
-  className?: string;  
+  className?: ClassValue;  
   }
 
 function ListContainer({
   header,
   render,
-  // path,
+  path,
   isLoading,
 }: ListContainerTypes) {
   return (
     <section className="space-y-7 px-4 py-10 md:px-12 dark:bg-gray-900">
       <header className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-semibold">{header}</h1>
-        {/* <Link to={path} className="text-md font-light text-blue-500">
+        <Link to={path} className="text-md font-light text-blue-500 hidden">
           See All
-        </Link> */}
+        </Link>
       </header>
 
       {isLoading ? (
